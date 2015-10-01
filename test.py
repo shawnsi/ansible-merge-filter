709bcbaab0f6a24711448a75b8290abf9609e882
Shawn Siefkas <shawn.siefkas@meredith.com>
2015-10-01 10:42:32 -0500

    Unit tests

---
+++ test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import sys
+import unittest
+
+sys.path.append('./filter_plugins')
+
+from merge import merge
+
+class TestMerge(unittest.TestCase):
+
+    def test_identity(self):
+        identity = {}
+
+        other = {}
+        merged = merge(identity, other)
+        self.assertDictEqual(merged, other)
+
+        other = {'foo': 1}
+        merged = merge(identity, other)
+        self.assertDictEqual(merged, other)
+
+        other = {'foo': {'bar': 1}}
+        merged = merge(identity, other)
+        self.assertDictEqual(merged, other)
+
+    def test_sum(self):
+        a, b = {'foo': 1}, {'foo': 1}
+        merged = merge(a, b)
+        self.assertDictEqual(merged, {'foo': 2})
+
+    def test_sum_recursion(self):
+        a, b = {'foo': {'bar': 1}}, {'foo': {'bar': 1}}
+        merged = merge(a, b)
+        self.assertDictEqual(merged, {'foo': {'bar': 2}})
+
+    def test_sum_lists(self):
+        a, b = {'foo': [1]}, {'foo': [1]}
+        merged = merge(a, b)
+        self.assertDictEqual(merge, {'foo': [1, 1]})
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
